ff6f393af7928d6ffac9cd83e12ed060e5d0340b
Lorenzo Ruffati <lorenzo.ruffati@unikie.com>
2022-09-19 10:30:45 +0300

    Streamline path generation for lock file

--- src/shm/shm.c
+++ src/shm/shm.c
@@ -7,16 +7,21 @@
 
 #define BASEPATHSHM "/tmp/shm_ocp/"
 
+char * lockpath_from_pass(char* passw){
+    char* path = malloc(strlen(passw) + strlen(BASEPATHSHM)+ strlen(".lock"));
+    strcpy(path, BASEPATHSHM);
+    strcat(path, passw);
+    strcat(path, ".lock");
+    return path;
+}
+
 int lock_sync_file(char *passw){
     struct stat st = {0};
     if (stat(BASEPATHSHM, &st) == -1) {
         mkdir(BASEPATHSHM, 0770);
     }
 
-    char* path = malloc(strlen(passw) + strlen(BASEPATHSHM)+ strlen(".lock"));
-    strcpy(path, BASEPATHSHM);
-    strcat(path, passw);
-    strcat(path, ".lock");
+    char* path = lockpath_from_pass(passw);
 
     int fd = open(path, O_CREAT, 0660);
     int res = flock(fd, LOCK_EX);
@@ -24,7 +29,14 @@ int lock_sync_file(char *passw){
         printf("%d %d\n", res, errno);
         err_and_leave("Error when locking file", 5);
     }
+    sleep(10);
+    unlink(path);
     return fd;
+}
+
+int unlink_lock(char *passw){
+    char* path = lockpath_from_pass(passw);
+    unlink(path);
 }
 
 
@@ -43,12 +55,7 @@ mmap_creat_ret_t init_or_create_mmap(char* passwd){
             err_and_leave("Error when accessing shared memory", 5);
         }
     }
-    sleep(1);
-    printf("Created shared mem");
-    sleep(1);
     ftruncate(fdm, sizeof(coord_struct));
-    printf("Truncated\n");
-    sleep(1);
     coord_struct* str = /*malloc(sizeof(coord_struct));/*/ (coord_struct*) mmap(NULL, sizeof(coord_struct), PROT_READ | PROT_WRITE, MAP_SHARED, fdm, 0);
     // TODO: init all locks and the like here, returning will relinquish the lock and others might access
     printf("%p\n", str);
@@ -59,7 +66,6 @@ mmap_creat_ret_t init_or_create_mmap(char* passwd){
     int ret = pthread_mutex_init(&(str->lock), &attr);
     printf("%d %d\n", ret, errno);
     return (mmap_creat_ret_t){str, fdm};
-
 }
 
 int use_shared(setting_t settings){
@@ -67,6 +73,7 @@ int use_shared(setting_t settings){
     mmap_creat_ret_t shm_info = init_or_create_mmap(settings.password);
 
     flock(fd, LOCK_UN);
+
     switch (settings.role)
     {
     case SENDER:
@@ -101,7 +108,7 @@ int shared_sender(setting_t settings, int lockfd, mmap_creat_ret_t mmap_info){
         }
         printf(":(\n");
         pthread_mutex_unlock(&shr_str->lock);
-        sleep(1);
+        sleep(0.5);
     }
 
 }
@@ -111,14 +118,14 @@ int shared_receiver(setting_t settings, int lockfd, mmap_creat_ret_t mmap_info){
 
     while (1){
         printf("Locking reader ...\n");
-        pthread_mutex_lock(&(shr_str->lock));
+        pthread_mutex_lock(&shr_str->lock);
         printf("I have lock\n");
         for (int i=3; i>0; i--){
             printf("%d\n", i);
             sleep(1);
         }
         printf(":)\n");
-        pthread_mutex_unlock(&(shr_str->lock));
-        sleep(1);
+        pthread_mutex_unlock(&shr_str->lock);
+        sleep(0.5);
     }
 }
